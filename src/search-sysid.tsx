f286d7bb9882842947cace900a430a3231c4333a
robertoalvarezalonso <roberto@swissflowit.com>
2025-04-28 10:35:46 +0200

    run script in background

--- src/search-sysid.tsx
+++ src/search-sysid.tsx
@@ -1,122 +1,106 @@
 import { LaunchProps, LocalStorage, showToast, Toast, open } from "@raycast/api";
 import { Instance } from "./types";
-
-var forbiddenPrefixes = ["ts_", "sysx_", "v_", "sys_rollback_", "pa_"];
+import { findSysID } from "./utils/snSnippets";
 
 export default async (props: LaunchProps) => {
   const { sys_id } = props.arguments;
-  const instance = await LocalStorage.getItem<string>("selected-instance");
-  if (!instance) {
+  const selectedInstance = await LocalStorage.getItem<string>("selected-instance");
+  if (!selectedInstance) {
     showToast(Toast.Style.Failure, "No instances found", "Please create an instance profile first");
     return;
   }
-  const instanceProfile = JSON.parse(instance) as Instance;
 
-  const token = await getServiceNowToken(instanceProfile);
-  console.log(token);
+  const instance = JSON.parse(selectedInstance) as Instance;
 
-  var script = `function test() {
-    gs.log("test")
+  showToast(Toast.Style.Animated, `Searching sys_id in ${instance.alias}...`);
+
+  const client = new ServiceNowClient(instance);
+  const isAuthenticated = await client.init();
+
+  if (!isAuthenticated) {
+    return;
   }
 
-  test();`;
+  const script = `${findSysID.toString()}
+  findSysID("${sys_id}");`;
 
-  snuStartBackgroundScript(instanceProfile, script, token);
-
-  const commonTables = ["sys_metadata", "task", "cmdb_ci", "sys_user", "kb_knowledge"];
-
-  const toast = showToast(Toast.Style.Animated, "Searching sys_id...");
-  try {
-    for (const tableName of commonTables) {
-      (await toast).message = tableName;
-      const result = await callTableAPI(instanceProfile, `${tableName}?sys_id=${sys_id}&sysparm_fields=sys_id`, token);
-      if (result) {
-        await open(`https://${instanceProfile.name}.service-now.com/${tableName}.do?sys_id=${sys_id}`);
-        return;
-      }
+  const callBack = (response: string) => {
+    const answer = response.match(/###(.*)###/);
+    if (response.length == 0) showToast(Toast.Style.Failure, "Could not search for sys_id. (are you an Admin?)");
+    else if (answer != null && answer[1]) {
+      var table = answer[1].split("^")[0];
+      var path = table + ".do?sys_id=" + sys_id;
+      open(`https://${instance.name}.service-now.com/${path}`);
+    } else {
+      showToast(Toast.Style.Failure, `sys_id not found on ${instance.alias}`);
     }
+  };
 
-    /* const tables = await callTableAPI(
-      instanceProfile,
-      "sys_db_object?sysparm_fields=name&sysparm_query=super_class=NULL^sys_update_nameISNOTEMPTY^nameNOT LIKE00^nameNOT LIKE$^nameNOT INsys_metadata,task,cmdb_ci,sys_user,kb_knowledge,cmdb_ire_partial_payloads_index^scriptable_table=false^ORscriptable_tableISEMPTY",
-    );
-    for (const table of tables) {
-      const tableName = table.name;
-      var hasForbiddenPrefix = forbiddenPrefixes.some(function (forbiddenPrefix) {
-        return tableName.startsWith(forbiddenPrefix);
-      });
-      if (!hasForbiddenPrefix) {
-        (await toast).message = tableName;
-        const result = await callTableAPI(instanceProfile, `${tableName}?sys_id=${sys_id}&sysparm_fields=sys_id`);
-        if (result) {
-          await open(`https://${instanceProfile.name}.service-now.com/${tableName}.do?sys_id=${sys_id}`);
-          return;
-        }
-      }
-    } */
-    showToast(Toast.Style.Failure, "Record not found", "The sys_id wasn't found");
-  } catch (error) {
-    showToast(Toast.Style.Failure, "Error", String(error));
-  }
+  await client.startBackgroundScript(script, callBack);
 };
 
-async function callTableAPI(instance: Instance, query: string, token) {
-  const url = `https://${instance.name}.service-now.com/api/now/table/${query}`;
+class ServiceNowClient {
+  private instance: Instance;
+  private sessionData: { ck: string; cookies: string } | null = null;
 
-  const response = await fetch(url, {
-    headers: {
-      //Authorization: `Basic ${Buffer.from(instance.username + ":" + instance.password).toString("base64")}`,
-      "X-userToken": token.data.ck,
-      Cookie: token.cookies,
-    },
-  });
-
-  const data = await response.json();
-  if (data.result && data.result.length > 0) return data.result;
-}
-
-async function getServiceNowToken(instance: Instance) {
-  const url = `https://${instance.name}.service-now.com/sn_devstudio_/v1/get_publish_info`;
-  console.log(url);
-
-  try {
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Basic ${Buffer.from(instance.username + ":" + instance.password).toString("base64")}`,
-      },
-    });
-
-    const data = await response.json();
-    const cookies = response.headers.get("set-cookie");
-    return { data, cookies };
-  } catch (error) {
-    console.error("Error getting ServiceNow token:", error);
-    throw error;
+  constructor(instance: Instance) {
+    this.instance = instance;
   }
-}
 
-function snuStartBackgroundScript(instance: Instance, script: string, token) {
-  const url = `https://${instance.name}.service-now.com/sys.scripts.do`;
+  async init(): Promise<boolean> {
+    this.sessionData = await this.authenticate();
+    return this.sessionData !== null;
+  }
 
-  const body = {
-    script: script,
-    runscript: "Run script",
-  };
-  console.log(body);
+  async authenticate() {
+    const url = `https://${this.instance.name}.service-now.com/sn_devstudio_/v1/get_publish_info`;
 
-  try {
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Cookie: token.cookies,
-        "X-UserToken": token.data.ck,
-      },
-      body: new URLSearchParams(body).toString(),
-    }).catch((error) => {
-      showToast(Toast.Style.Failure, "Script failed");
-    });
-  } catch (error) {
-    showToast(Toast.Style.Failure, "Script failed");
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Basic ${Buffer.from(this.instance.username + ":" + this.instance.password).toString("base64")}`,
+        },
+      });
+
+      const data = await response.json();
+      const cookies = response.headers.get("set-cookie");
+      return { ck: data.ck, cookies: cookies || "" };
+    } catch (error) {
+      console.error("Authentication Failed:", error);
+      showToast(
+        Toast.Style.Failure,
+        "Authentication Failed",
+        "Please verify your instance credentials and network connection",
+      );
+      return null;
+    }
+  }
+
+  async startBackgroundScript(script: string, callback: (data: string) => void) {
+    if (!this.sessionData) throw new Error("Not authenticated");
+    const url = `https://${this.instance.name}.service-now.com/sys.scripts.do`;
+
+    const body = {
+      script: script,
+      runscript: "Run script",
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Cookie: this.sessionData.cookies,
+          "X-UserToken": this.sessionData.ck,
+        },
+        body: new URLSearchParams(body).toString(),
+      });
+
+      const data = await response.text();
+      callback(data);
+    } catch (error) {
+      console.error("Background Script failed:", error);
+      showToast(Toast.Style.Failure, "Background Script failed");
+    }
   }
 }
